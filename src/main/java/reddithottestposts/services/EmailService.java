5731da7b8fb107c2d3708da7d7f869d0b607be95
Claudiu Cismaru <claudiu.cismaru94@gmail.com>
2019-04-21 21:35:49 +0300

    Try sendgrid

--- src/main/java/reddithottestposts/services/EmailService.java
+++ src/main/java/reddithottestposts/services/EmailService.java
@@ -3,6 +3,7 @@ package reddithottestposts.services;
 import com.github.jreddit.oauth.exception.RedditOAuthException;
 import com.github.jreddit.parser.entity.Submission;
 import com.github.jreddit.parser.exception.RedditParseException;
+import com.sendgrid.*;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import reddithottestposts.repos.UserRedditPostRepository;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -29,6 +31,9 @@ public class EmailService {
     private final Config config = ConfigFactory.load();
     private final String senderEmail = config.getString("sender-email");
     private final String senderPassword = config.getString("sender-password");
+    private final String sendgridApyKey = config.getString("sendgrid-api-key");
+
+    private final SendGrid sg = new SendGrid(sendgridApyKey);
 
     public void sendEmailWithRandomPost(String recipient) throws RedditOAuthException, RedditParseException {
         List<String> subreddits = Arrays.asList("hearthstone", "summonerschool", "jaxmains", "tryndameremains", "rivenmains", "scala");
@@ -77,36 +82,64 @@ public class EmailService {
         return props;
     }
 
+    private void sendEmail(String subject, String body, String recipient) {
+        System.out.println("Sending email with SENDGRID");
+
+        Email from = new Email("no-reply@awesome.com");
+        Email to = new Email(recipient);
+        Content content = new Content("text/html", body);
+        Mail mail = new Mail(from, subject, to, content);
+
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
     public void sendEmailWithSubmission(Submission submission, String recipient) {
         Properties props = setProps();
 
-        try {
-            Session session = Session.getDefaultInstance(props,
-                    new Authenticator(){
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(senderEmail, senderPassword);
-                        }});
+//        try {
+//            Session session = Session.getDefaultInstance(props,
+//                    new Authenticator(){
+//                        protected PasswordAuthentication getPasswordAuthentication() {
+//                            return new PasswordAuthentication(senderEmail, senderPassword);
+//                        }});
+//
+//            // -- Create a new message --
+//            Message msg = new MimeMessage(session);
+//
+//            // -- Set the FROM and TO fields --
+//            msg.setFrom(new InternetAddress(senderEmail));
+//            msg.setRecipients(Message.RecipientType.TO,
+//                    InternetAddress.parse(recipient,false));
+//            msg.setSubject("Check out this awesome reddit post!");
+//
+//            String text = getEmailBody(submission);
+//
+//            msg.setContent(text, "text/html");
+//            msg.setSentDate(new Date());
+//
+//            Transport.send(msg);
+//
+//            System.out.println("Message sent.");
+//        } catch (MessagingException e)
+//        {
+//            System.out.println("Error, cause: " + e);
+//        }
 
-            // -- Create a new message --
-            Message msg = new MimeMessage(session);
+        String body = getEmailBody(submission);
+        String subject = "Here is a cool reddit post for you!";
 
-            // -- Set the FROM and TO fields --
-            msg.setFrom(new InternetAddress(senderEmail));
-            msg.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(recipient,false));
-            msg.setSubject("Check out this awesome reddit post!");
-
-            String text = getEmailBody(submission);
-
-            msg.setContent(text, "text/html");
-            msg.setSentDate(new Date());
-
-            Transport.send(msg);
-
-            System.out.println("Message sent.");
-        } catch (MessagingException e)
-        {
-            System.out.println("Error, cause: " + e);
-        }
+        sendEmail(subject, body, recipient);
     }
 }
